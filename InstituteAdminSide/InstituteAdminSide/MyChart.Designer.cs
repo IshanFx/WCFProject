111a2cc4398660ce7570b6ab81c19772d818c93e
suresh <555sureshsri@gmail.com>
2015-03-22 23:46:25 +0530

    Report chart data added

--- InstituteAdminSide/InstituteAdminSide/MyChart.Designer.cs
+++ InstituteAdminSide/InstituteAdminSide/MyChart.Designer.cs
@@ -35,38 +35,40 @@ namespace InstituteAdminSide
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartIncomePart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chartTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncomePart)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // chart1
+            // chartIncomePart
             // 
             chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            this.chartIncomePart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 36);
-            this.chart1.Name = "chart1";
-            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            this.chartIncomePart.Legends.Add(legend1);
+            this.chartIncomePart.Location = new System.Drawing.Point(12, 36);
+            this.chartIncomePart.Name = "chartIncomePart";
+            this.chartIncomePart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
             series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
             series1.BorderWidth = 0;
             series1.ChartArea = "ChartArea1";
             series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             series1.Legend = "Legend1";
             series1.Name = "Income";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(920, 463);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            this.chartIncomePart.Series.Add(series1);
+            this.chartIncomePart.Size = new System.Drawing.Size(920, 463);
+            this.chartIncomePart.TabIndex = 0;
+            this.chartIncomePart.Text = "chart1";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.chartTitle);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Location = new System.Drawing.Point(-1, -3);
@@ -74,25 +76,36 @@ namespace InstituteAdminSide
             this.panel1.Size = new System.Drawing.Size(1010, 583);
             this.panel1.TabIndex = 1;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Controls.Add(this.chartIncomePart);
+            this.panel2.Location = new System.Drawing.Point(1, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(946, 511);
+            this.panel2.TabIndex = 44;
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(28, 12);
+            this.label6.Location = new System.Drawing.Point(294, 12);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(369, 57);
             this.label6.TabIndex = 43;
             this.label6.Text = "Income Chart";
             // 
-            // panel2
+            // chartTitle
             // 
-            this.panel2.BackColor = System.Drawing.Color.DimGray;
-            this.panel2.Controls.Add(this.chart1);
-            this.panel2.Location = new System.Drawing.Point(1, 72);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(946, 511);
-            this.panel2.TabIndex = 44;
+            this.chartTitle.AutoSize = true;
+            this.chartTitle.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chartTitle.ForeColor = System.Drawing.Color.White;
+            this.chartTitle.Location = new System.Drawing.Point(25, 12);
+            this.chartTitle.Name = "chartTitle";
+            this.chartTitle.Size = new System.Drawing.Size(263, 57);
+            this.chartTitle.TabIndex = 45;
+            this.chartTitle.Text = "Employee";
             // 
             // MyChart
             // 
@@ -103,7 +116,7 @@ namespace InstituteAdminSide
             this.Name = "MyChart";
             this.Text = "Chart";
             this.Load += new System.EventHandler(this.Chart_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncomePart)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -113,9 +126,10 @@ namespace InstituteAdminSide
 
         #endregion
 
-        private Chart chart1;
+        private Chart chartIncomePart;
         private Panel panel1;
         private Label label6;
         private Panel panel2;
+        private Label chartTitle;
     }
 }
